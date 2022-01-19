091ee702d00dfbde383990dd00dc0201f4618f82
James <kingjamesegun@gmail.com>
2022-01-19 10:27:07 +0100

    Navbar fixed

--- src/compoents/Navbar.tsx
+++ src/compoents/Navbar.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, MouseEvent } from 'react';
 import Logo from '../assets/Logo.svg';
 import Bar from '../assets/bar.svg';
+import CloseBar from '../assets/close.svg';
 import '../styles/Navbar.css';
 
 function Navbar() {
+	const [toogle, setToggle] = useState(false);
+
+	const handleBarClick = (e: MouseEvent) => {
+		e.preventDefault();
+		setToggle((prevState) => !prevState);
+	};
+	let toogleClass = 'nav__links';
+
+	if (toogle === true) {
+		toogleClass += ' active';
+	}
+
 	return (
 		<nav className='navbar'>
 			<div className='nav__text'>
 				<img src={Logo} alt='Logo' />
 			</div>
-			<div className='nav__bar'>
-				<img src={Bar} alt='Bar' />
+			<div className='nav__bar' onClick={handleBarClick}>
+				{toogle ? (
+					<img src={CloseBar} alt='close' />
+				) : (
+					<img src={Bar} alt='Bar' />
+				)}
 			</div>
-			<div className='nav__links'>
+			<div className={toogleClass}>
 				<ul className='nav__ul'>
 					<li className='nav__li'>
 						<a href='#'>Contact US</a>
