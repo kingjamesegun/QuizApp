6398772c634c9d5a99bb5a545756425ddc553bd7
James <kingjamesegun@gmail.com>
2022-01-22 23:23:34 +0100

    Router set

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import './App.css';
 import Navbar from './compoents/Navbar';
 import HomePage from './Pages/HomePage';
+import QuizPage from './Pages/QuizPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+	const startTrivia = () => {};
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
-  const startTrivia =()=>{
-
-  }
-  const checkAnswer =(e: React.MouseEvent<HTMLButtonElement>)=>{
-
-  }
-
-  const nextQuestion=()=>{
-    
-  }
-  return (
-    <div className=''>
-      <Navbar/>
-      <HomePage/>
-    </div>
-  );
+	const nextQuestion = () => {};
+	return (
+		<Router>
+			<Navbar />
+			<Route path='/' exact>
+				<HomePage />
+			</Route>
+			<Route path='/quiz'>
+				<QuizPage />
+			</Route>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/Pages/QuizPage.tsx
@@ -0,0 +1,23 @@
+import React, {MouseEvent} from 'react';
+
+function QuizPage() {
+    // ftn start the quiz
+    const startTriva = async ()=>{
+
+    }
+
+    // BTN ftn that shows answer
+    const checkAnswer = (e: MouseEvent<HTMLButtonElement>)=>{
+
+    }
+
+	return (
+        <div className='quiz'>
+            {/* https://opentdb.com/api.php?amount=10&type=multiple */}
+
+        </div>
+    )
+    ;
+}
+
+export default QuizPage;
